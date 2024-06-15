772d13c834b4e07304e217e545739cd6ec6ee288
Raja <Rajasekhar.iosswift@gmail.com>
2024-06-15 11:23:10 +0530

    Initial Commit

---
+++ SwiftUI_Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI_Basics
+//
+//  Created by rajasekhar pidikiti on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
