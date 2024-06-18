e63a2fd27abd578ed296087f8d8b5c82900ede3e
Raja <Rajasekhar.iosswift@gmail.com>
2024-06-18 14:22:46 +0530

    Added function to return a view

--- SwiftUI_Basics/SwiftUI_Padding_Space.swift
+++ SwiftUI_Basics/SwiftUI_Padding_Space.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct SwiftUI_Padding_Space: View {
+    
+    var body: some View {
+        basicPading()
+    }
+    
+    fileprivate func basicPading() -> some View {
+        return BasicPading()
+    }
+}
+
+struct BasicPading: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .font(.largeTitle)
             .fontWeight(.heavy)
             .foregroundColor(Color.white)
             .multilineTextAlignment(.center)
-            .padding(.all)
+        //.padding()
+            .padding(.top, 10)
+        //.padding(.all, 10)
+        //.frame(width: 300, height: 100, alignment: .center)
             .background(.purple)
-            .frame(width: 400, height: 100, alignment: .center)
+            .frame(width: 300, height: 100, alignment: .top)
+            .padding(.vertical, 15)
             .background(.green)
-            
     }
 }
 
